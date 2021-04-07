5c716d0e01dfcbf6a0d3994b58b62e25dd58b247
lxndrdodson <72848736+lxndrdodson@users.noreply.github.com>
2021-04-06 22:26:25 -0500

    Update AssociateProfitSplitter.sol

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -1,6 +1,61 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
+pragma solidity ^0.5.0;
+
+// Equal Split
+contract AssociateProfitSplitter {
+    
+    // Create payable address for Employee One
+    address payable employee_one; 
+    
+    // Create payable address for Employee Two
+    address payable employee_two;
+    
+    // Create payable address for Employee Three
+    address payable employee_three;
+    
+    // Create Constructor
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        
+        // Setting Employee One
+        employee_one = _one;
+        
+        // Setting Employee Two
+        employee_two = _two;
+        
+        // Setting Employee Three
+        employee_three = _three;
+    }
+
+    // Create Balance function
+    function balance() public view returns(uint) {
+        
+        // Return Balance
+        return address(this).balance;
+    }
+
+    // Create deposit function
+    function deposit() public payable {
+        
+        // Split `msg.value` into three
+        uint amount = msg.value/3; 
+
+        // Transfer 1/3 of the amount to Employee one
+        employee_one.transfer(amount);
+        
+        // Transfer 1/3 of the amount to Employee two
+        employee_two.transfer(amount);
+        
+        // Transfer 1/3 of the amount to Employee three
+        employee_three.transfer(amount);
+
+        // Send Remainder to HR
+       uint remainder = msg.value-amount*3;
+       msg.sender.transfer(remainder);
+    }
+
+    // Create Fallback function
+    function() external payable {
+        
+        // Enforce Deposit function
+        deposit();
+    }
 }
